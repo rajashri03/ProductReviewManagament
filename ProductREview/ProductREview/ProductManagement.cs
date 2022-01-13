0e7590ccf6a14767a83cd18b8424c142993d1f26
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-13 15:24:03 +0530

    [Rajashri]Add:Retrive data from list whoes rating is greater than 3

--- ProductREview/ProductREview/ProductManagement.cs
+++ ProductREview/ProductREview/ProductManagement.cs
@@ -15,7 +15,24 @@ namespace ProductREview
         public static void Top3Records(List<ProductReview> productreviewlist)
         {
             var productData = (from productReviews in productreviewlist
-                                select productReviews).Take(3);
+                               select productReviews).Take(3);
+
+            foreach (var list in productData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "IsLike:- " + list.Islike);
+            }
+        }
+        /// <summary>
+        /// Retrive all data from list whose rating greater than 3
+        /// </summary>
+        /// <param name="productreviewlist"></param>
+        public static void RetriveRecords(List<ProductReview> productreviewlist)
+        {
+            var productData = (from productReviews in productreviewlist
+                               where productReviews.ProductID == 1 ||  productReviews.ProductID == 4 || productReviews.ProductID == 9
+                               && productReviews.Rating >3
+                               select productReviews);
 
             foreach (var list in productData)
             {
--- ProductREview/ProductREview/Program.cs
+++ ProductREview/ProductREview/Program.cs
@@ -6,18 +6,18 @@ Console.WriteLine("***************Product Review Management********************"
 //List for product review
 List<ProductReview> productReviewList = new List<ProductReview>()
 {
-    new ProductReview(){ProductID=1,UserID=1,Rating=5,Review="Bad",Islike=false},
+    new ProductReview(){ProductID=1,UserID=1,Rating=9,Review="Bad",Islike=false},
     new ProductReview(){ProductID=2,UserID=1,Rating=4,Review="Good",Islike=true},
     new ProductReview(){ProductID=3,UserID=1,Rating=5,Review="Good",Islike=true},
     new ProductReview(){ProductID=4,UserID=1,Rating=6,Review="Good",Islike=false},
-    new ProductReview(){ProductID=5,UserID=1,Rating=2,Review="Very Good",Islike=true},
+    new ProductReview(){ProductID=5,UserID=1,Rating=5,Review="Very Good",Islike=true},
     new ProductReview(){ProductID=6,UserID=1,Rating=1,Review="Bad",Islike=false},
     new ProductReview(){ProductID=7,UserID=1,Rating=10,Review="Good",Islike=false},
     new ProductReview(){ProductID=8,UserID=1,Rating=9,Review="Very Nice",Islike=true},
     new ProductReview(){ProductID=9,UserID=1,Rating=10,Review="Nice",Islike=true},
     new ProductReview(){ProductID=10,UserID=1,Rating=8,Review="Nice",Islike=true},
 };
-Console.WriteLine("Select \n1)CreateProductReview\n2)Retrive Top 3 Review");
+Console.WriteLine("Select \n1)CreateProductReview\n2)Retrive Top 3 Review\n3)retrive record whose rating >3");
 int option = Convert.ToInt16(Console.ReadLine());
 switch(option)
 {
@@ -31,5 +31,11 @@ switch(option)
         break;
     case 2:
         ProductManagement.Top3Records(productReviewList);
+        break;
+    case 3:
+        ProductManagement.RetriveRecords(productReviewList);
+        break;
+   default:
+        Console.WriteLine("Wrong Choice");
         break;
 }
